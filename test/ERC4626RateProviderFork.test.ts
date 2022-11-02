c97a106ff876317fa22199506cd469c0c1d8bea1
mkflow27 <35266877+mkflow27@users.noreply.github.com>
2022-11-02 17:31:03 +0100

    reworked test to account for RateProvider changes

--- test/ERC4626RateProviderFork.test.ts
+++ test/ERC4626RateProviderFork.test.ts
@@ -13,13 +13,13 @@ describe('mainnet fork ERC4626 rate provider', function () {
     // mainnet fork from block 15868474
     // returns 1001216513304948175 as per the upper block
     const Erc4626RateProvider = await ethers.getContractFactory('ERC4626RateProvider');
-    erc4626RateProvider = await Erc4626RateProvider.deploy('0xac3E018457B222d93114458476f3E3416Abbe38F');
+    erc4626RateProvider = await Erc4626RateProvider.deploy(
+      '0xac3E018457B222d93114458476f3E3416Abbe38F',
+      '0x5E8422345238F34275888049021821E8E08CAa1f'
+    );
   });
 
   it('returns rate from stakedFraxEther', async () => {
     expect(await erc4626RateProvider.getRate()).to.equal(ethers.BigNumber.from('1001216513304948175'));
-  });
-  it('checks decimals are the same', async () => {
-    expect(await erc4626RateProvider.vaultDecimals()).to.equal(18);
   });
 });
