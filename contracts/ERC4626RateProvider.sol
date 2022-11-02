6c05f1af7df43eac51d4100e855c0aa03522fa95
mkflow27 <35266877+mkflow27@users.noreply.github.com>
2022-11-02 19:02:58 +0100

    changes from tom

--- contracts/ERC4626RateProvider.sol
+++ contracts/ERC4626RateProvider.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.7.0;
 
 import "@balancer-labs/v2-solidity-utils/contracts/math/Math.sol";
+import "@balancer-labs/v2-solidity-utils/contracts/math/FixedPoint.sol";
 import "@balancer-labs/v2-interfaces/contracts/solidity-utils/misc/IERC4626.sol";
 import "@balancer-labs/v2-interfaces/contracts/solidity-utils/openzeppelin/IERC20.sol";
 import "./interfaces/IERC20Extended.sol";
@@ -13,9 +14,10 @@ import "./interfaces/IRateProvider.sol";
  * @dev ensure `getRate` returns scaled to 1e18
  */
 contract ERC4626RateProvider is IRateProvider {
-    using Math for uint256;
-
+    using FixedPoint for uint256;
+    
     uint256 private immutable _rateScaleFactor;
+    IERC20 public immutable mainToken;
     IERC4626 public immutable wrappedToken;
 
     constructor(IERC4626 _wrappedToken, IERC20Extended _mainToken) {
@@ -37,8 +39,11 @@ contract ERC4626RateProvider is IRateProvider {
         
         uint256 wrappedTokenDecimals = IERC20Extended(address(_wrappedToken)).decimals();
         uint256 mainTokenDecimals = _mainToken.decimals();
+
+        require(mainTokenDecimals <= 18 && wrappedTokenDecimals <= 18, "Only tokens with 18 or fewer decimals are supported");
         uint256 digitsDifference = Math.add(18, wrappedTokenDecimals).sub(mainTokenDecimals);
         _rateScaleFactor = 10**digitsDifference;
+        mainToken = _mainToken;
         wrappedToken = _wrappedToken;
     }
 
@@ -47,12 +52,12 @@ contract ERC4626RateProvider is IRateProvider {
      * @dev scaling to 1e18 must be ensured
      */
     function getRate() external view override returns (uint256) {
-        uint256 assetsPerShare = wrappedToken.convertToAssets(1e18);
+        uint256 assetsPerShare = wrappedToken.convertToAssets(FixedPoint.ONE);
 
         // This function returns a 18 decimal fixed point number
         // assetsPerShare decimals:   18 + main - wrapped
         // _rateScaleFactor decimals: 18 - main + wrapped
-        uint256 rate = assetsPerShare.mul(_rateScaleFactor).divDown(1e18);
+        uint256 rate = assetsPerShare.mulDown(_rateScaleFactor);
         return rate;
     }
 }
