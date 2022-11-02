c46c8527f84d92d06731fa1cf95fe9a92e1b93c6
mkflow27 <35266877+mkflow27@users.noreply.github.com>
2022-11-02 09:54:20 +0100

    setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,16 @@ import { HardhatUserConfig } from 'hardhat/config';
 
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
+import "@nomiclabs/hardhat-etherscan";
+//import 'hardhat-local-networks-config-plugin';
+
+const CHAIN_IDS = {
+  hardhat: 31337, // chain ID for hardhat testing
+};
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.11',
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
@@ -13,6 +19,24 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  etherscan: {
+    apiKey: {
+      goerli: 'etherscan key'
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: CHAIN_IDS.hardhat,
+      forking: {
+        url: `archival node`,
+        blockNumber: 15868474,
+      }
+    },
+    goerli: {
+      url: "goerli node",
+      accounts: ["pk"],
+    }
+  }
 };
 
 export default config;
