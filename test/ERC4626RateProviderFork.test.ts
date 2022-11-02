9b83473d2a148ff0943ab066caae6556b9d22fd1
mkflow27 <35266877+mkflow27@users.noreply.github.com>
2022-11-02 18:01:43 +0100

    clarity

--- test/ERC4626RateProviderFork.test.ts
+++ test/ERC4626RateProviderFork.test.ts
@@ -14,8 +14,8 @@ describe('mainnet fork ERC4626 rate provider', function () {
     // returns 1001216513304948175 as per the upper block
     const Erc4626RateProvider = await ethers.getContractFactory('ERC4626RateProvider');
     erc4626RateProvider = await Erc4626RateProvider.deploy(
-      '0xac3E018457B222d93114458476f3E3416Abbe38F',
-      '0x5E8422345238F34275888049021821E8E08CAa1f'
+      '0xac3E018457B222d93114458476f3E3416Abbe38F', // Staked Frax Ether
+      '0x5E8422345238F34275888049021821E8E08CAa1f', // Frax Ether
     );
   });
 
